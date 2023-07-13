52f3729619e1c213989ca57de1d0636847318552
Rahul <cse.rahul19@gmail.com>
2023-07-13 05:36:33 +0530

    added test command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """discord bot module"""
 from src.client import MyClient
+from src.bot.commands import Test
 
 if __name__ == "__main__":
     MyClient.runClient()
--- src/client.py
+++ src/client.py
@@ -1,28 +1,27 @@
-from discord import Client
+import discord
+from discord.app_commands import CommandTree
 from discord.flags import Intents
+from discord.guild import Guild
+from discord.ext import commands
 from .secrets import Secrets
+from .bot.commands import Test, test
 
 
-class MyClient(Client):
+class MyClient(discord.Client):
     intents: Intents = None
     secrets: Secrets = None
+    bot: commands.Bot = None
 
-    def __init__(self, intents: Intents = None, secrets: Secrets = None) -> None:
-        if intents:
-            self.intents = intents
-        else:
-            self.intents = Intents.default()
-            self.intents.message_content = True
-
-        if secrets:
-            self.secrets = secrets
-        else:
-            self.secrets = Secrets()
-
+    def __init__(self) -> None:
+        self.intents = Intents.default()
+        self.intents.message_content = True
+        self.secrets = Secrets()
         super().__init__(intents=self.intents)
+        self.bot = commands.Bot(command_prefix=">", intents=self.intents)
 
     async def on_ready(self):
         """initialization log"""
+        await self.sync(guild=discord.Object)
         print(f"Logged in as {self.user} ")
 
     async def on_message(self, message):
@@ -32,4 +31,7 @@ class MyClient(Client):
     @staticmethod
     def runClient():
         client: MyClient = MyClient()
-        client.run(token=client.secrets.token)
+        client.bot.add_command(test)
+        # client.bot.add_cog(Test(client.bot))
+        # client.bot.get_cog("test")
+        client.bot.run(token=client.secrets.token)
